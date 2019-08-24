5a426a5d82ad563793ebcf222a5128e880f4ca21
Varun <varunsridharan23@gmail.com>
2019-08-24 07:18:58 +0530

    first commit

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM php:7.3
+
+RUN git config --global user.email "githubactionbot+wp@gmail.com" && git config --global user.name "WP Textdomain Github Bot"
+
+COPY entrypoint.sh /entrypoint.sh
+COPY add-textdomain.php add-textdomain.php/
+
+RUN chmod +x /entrypoint.sh
+RUN chmod +x /add-textdomain.php
+
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -eo
+
+BRANCH=${GITHUB_REF#refs/heads/}
+DOMAIN="$INPUT_DOMAIN"
+
+if [[ -z "$GITHUB_TOKEN" ]]; then
+	echo "Set the GITHUB_TOKEN env variable"
+	exit 1
+fi
+
+cd ../
+
+php addtextdomain.php -i "$DOMAIN" "$GITHUB_WORKSPACE"
+
+cd "$GITHUB_WORKSPACE"
+
+# Add File To Commit
+git add -A
+
+# Add Commit Message
+git commit -m "WordPress Textdomains Updated"
+
+# Push To Github
+git push "https://x-access-token:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY" HEAD:$BRANCH
